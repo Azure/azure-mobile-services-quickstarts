20423cedaf497af39c3cc102ee415c0c0904f138
Paul Batum <paul.batum@gmail.com>
2014-05-27 15:32:29 -0700

    Fix comment in quickstarts

--- Quickstart/ZUMOAPPNAMEService/Controllers/TodoItemController.cs
+++ Quickstart/ZUMOAPPNAMEService/Controllers/TodoItemController.cs
@@ -36,7 +36,7 @@ namespace ZUMOAPPNAMEService.Controllers
             return UpdateAsync(id, patch);
         }
 
-        // POST tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        // POST tables/TodoItem
         public async Task<IHttpActionResult> PostTodoItem(TodoItem item)
         {
             TodoItem current = await InsertAsync(item);
