583d33bccbe2912a1202ad8a218716dd66f9358c
Henrik Frystyk Nielsen <henrikn@microsoft.com>
2014-09-21 13:53:51 -0700

    Added missing assembly redirect and fixed quick start by adding missing using statement.

--- Quickstart/ZUMOAPPNAMEService/App_Start/WebApiConfig.cs
+++ Quickstart/ZUMOAPPNAMEService/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Web.Http;
 using Microsoft.WindowsAzure.Mobile.Service;
