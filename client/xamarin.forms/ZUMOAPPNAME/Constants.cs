da4c73a24fcf953db7716bc5f11cccc5304a0f60
Fabio Cavalcante <facaval@microsoft.com>
2015-08-06 10:32:18 -0700

    Adding offline support to Xamarin.Forms

--- client/xamarin.forms/ZUMOAPPNAME/Constants.cs
+++ client/xamarin.forms/ZUMOAPPNAME/Constants.cs
@@ -4,9 +4,9 @@ namespace ZUMOAPPNAME
 {
 	public static class Constants
 	{
-		// Replace strings with your mobile services url and key.
-		public static string ApplicationURL = @"ZUMOAPPURL";
-		public static string ApplicationKey = @"ZUMOAPPKEY";
+		// Replace strings with your mobile services and gateway URLs.
+		public static string ApplicationURL = @"ZUMOAPPURL"; 
+        public static string ApplicationKey = @"ZUMOAPPKEY"; 
 	}
 }
 
