9d527da2c4f5625746c4f045ddb93b7a15a23fa1
Fabio Cavalcante <facaval@microsoft.com>
2015-08-06 10:32:18 -0700

    Adding offline support to Xamarin.Forms

--- client/xamarin.forms/ZUMOAPPNAME/Constants.cs
+++ client/xamarin.forms/ZUMOAPPNAME/Constants.cs
@@ -4,9 +4,8 @@ namespace ZUMOAPPNAME
 {
 	public static class Constants
 	{
-		// Replace strings with your mobile services url and key.
+		// Replace strings with your mobile services and gateway URLs.
 		public static string ApplicationURL = @"ZUMOAPPURL";
-		public static string ApplicationKey = @"ZUMOAPPKEY";
 		public static string GatewayURL = @"ZUMOGATEWAYURL";
 	}
 }
--- client/xamarin.forms/ZUMOAPPNAME/TodoList.xaml.cs
+++ client/xamarin.forms/ZUMOAPPNAME/TodoList.xaml.cs
@@ -1,88 +1,158 @@
 ﻿using System;
-using System.Collections.Generic;
-
-using Xamarin.Forms;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace ZUMOAPPNAME
 {
-	public partial class TodoList : ContentPage
-	{
-		TodoItemManager manager;
+    public partial class TodoList : ContentPage
+    {
+        TodoItemManager manager;
 
-		public TodoList ()
-		{
-			InitializeComponent ();
+        public TodoList()
+        {
+            InitializeComponent();
 
-			manager = new TodoItemManager ();
-		}
+            manager = new TodoItemManager();
 
-		protected override async void OnAppearing ()
-		{
-			base.OnAppearing ();
+            // OnPlatform<T> doesn't currently support the "Windows" target platform, so we have this check here.
+            if (Device.OS == TargetPlatform.Windows || Device.OS == TargetPlatform.WinPhone)
+            {
+                syncButton.IsVisible = true;
+            }
+        }
 
-			todoList.ItemsSource = await manager.GetTodoItemsAsync ();
-		}
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
 
-		// Data methods
+            // Set syncItems to true in order to synchronize the data on startup when running in offline mode
+            await RefreshItems(true, syncItems: false);
+        }
 
-		async Task AddItem (TodoItem item) {
-			await manager.SaveTaskAsync(item);
-			todoList.ItemsSource = await manager.GetTodoItemsAsync ();
-		}
-		async Task DeleteItem (TodoItem item) {
-			await manager.DeleteTaskAsync(item);
-			todoList.ItemsSource = await manager.GetTodoItemsAsync ();
-		}
+        // Data methods
+        async Task AddItem(TodoItem item)
+        {
+            await manager.SaveTaskAsync(item);
+            todoList.ItemsSource = await manager.GetTodoItemsAsync();
+        }
 
-		public async void OnAdd (object sender, EventArgs e) {
-			var todo = new TodoItem { Name = newItemName.Text };
-			await AddItem (todo);
-			newItemName.Text = "";
-			newItemName.Unfocus ();
-		}
+        async Task CompleteItem(TodoItem item)
+        {
+            item.Done = true;
+            await manager.SaveTaskAsync(item);
+            todoList.ItemsSource = await manager.GetTodoItemsAsync();
+        }
 
-		// Event handlers
+        public async void OnAdd(object sender, EventArgs e)
+        {
+            var todo = new TodoItem { Name = newItemName.Text };
+            await AddItem(todo);
 
-		public async void OnSelected (object sender, SelectedItemChangedEventArgs e) {
-			var todo = e.SelectedItem as TodoItem;
-			if (Device.OS != TargetPlatform.iOS && todo != null) {
-				// Not iOS - the swipe-to-delete is discoverable there
-				if (Device.OS == TargetPlatform.Android) {
-					await DisplayAlert (todo.Name, "Press-and-hold to delete task " + todo.Name, "Got it!");
-				} else {
-					// Windows, not all platforms support the Context Actions yet
-					if (await DisplayAlert ("Delete?", "Do you wish to delete " + todo.Name + "?", "Delete", "Cancel")) {
-						await DeleteItem (todo);
-					}
-				}
-			}
-			// prevents background getting highlighted
-			todoList.SelectedItem = null;
-		}
+            newItemName.Text = string.Empty;
+            newItemName.Unfocus();
+        }
 
-		// http://developer.xamarin.com/guides/cross-platform/xamarin-forms/working-with/listview/#context
-		public async void OnDelete (object sender, EventArgs e) {
-			var mi = ((MenuItem)sender);
-			var todo = mi.CommandParameter as TodoItem;
-			await DeleteItem (todo);
-		}
+        // Event handlers
+        public async void OnSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var todo = e.SelectedItem as TodoItem;
+            if (Device.OS != TargetPlatform.iOS && todo != null)
+            {
+                // Not iOS - the swipe-to-delete is discoverable there
+                if (Device.OS == TargetPlatform.Android)
+                {
+                    await DisplayAlert(todo.Name, "Press-and-hold to complete task " + todo.Name, "Got it!");
+                }
+                else
+                {
+                    // Windows, not all platforms support the Context Actions yet
+                    if (await DisplayAlert("Mark completed?", "Do you wish to complete " + todo.Name + "?", "Complete", "Cancel"))
+                    {
+                        await CompleteItem(todo);
+                    }
+                }
+            }
+            // prevents background getting highlighted
+            todoList.SelectedItem = null;
+        }
 
-		// http://developer.xamarin.com/guides/cross-platform/xamarin-forms/working-with/listview/#pulltorefresh
-		public async void OnRefresh (object sender, EventArgs e) {
-			var list = (ListView)sender;
+        // http://developer.xamarin.com/guides/cross-platform/xamarin-forms/working-with/listview/#context
+        public async void OnComplete(object sender, EventArgs e)
+        {
+            var mi = ((MenuItem)sender);
+            var todo = mi.CommandParameter as TodoItem;
+            await CompleteItem(todo);
+        }
+
+        // http://developer.xamarin.com/guides/cross-platform/xamarin-forms/working-with/listview/#pulltorefresh
+        public async void OnRefresh(object sender, EventArgs e)
+        {
+            var list = (ListView)sender;
             var success = false;
-			try {
-				todoList.ItemsSource = await manager.GetTodoItemsAsync ();
+            try
+            {
+                await RefreshItems(false, true);
                 success = true;
-			} catch (Exception ex) {
+            }
+            catch (Exception ex)
+            {
                 // requires C# 6
-				//await DisplayAlert ("Refresh Error", "Couldn't refresh data ("+ex.Message+")", "OK");
-			}
-			list.EndRefresh();
+                //await DisplayAlert ("Refresh Error", "Couldn't refresh data ("+ex.Message+")", "OK");
+            }
+            list.EndRefresh();
             if (!success)
                 await DisplayAlert("Refresh Error", "Couldn't refresh data", "OK");
-		}
-	}
+        }
+
+        public async void OnSyncItems(object sender, EventArgs e)
+        {
+            await RefreshItems(true, true);
+        }
+
+        private async Task RefreshItems(bool showActivityIndicator, bool syncItems)
+        {
+            using (var scope = new ActivityIndicatorScope(syncIndicator, showActivityIndicator))
+            {
+                todoList.ItemsSource = await manager.GetTodoItemsAsync(syncItems);
+            }
+        }
+
+        private class ActivityIndicatorScope : IDisposable
+        {
+            private bool showIndicator;
+            private ActivityIndicator indicator;
+            private Task indicatorDelay;
+
+            public ActivityIndicatorScope(ActivityIndicator indicator, bool showIndicator)
+            {
+                this.indicator = indicator;
+                this.showIndicator = showIndicator;
+
+                if (showIndicator)
+                {
+                    indicatorDelay = Task.Delay(2000);
+                    SetIndicatorActivity(true);
+                }
+                else
+                {
+                    indicatorDelay = Task.FromResult(0);
+                }
+            }
+
+            private void SetIndicatorActivity(bool isActive)
+            {
+                this.indicator.IsVisible = isActive;
+                this.indicator.IsRunning = isActive;
+            }
+
+            public void Dispose()
+            {
+                if (showIndicator)
+                {
+                    indicatorDelay.ContinueWith(t => SetIndicatorActivity(false), TaskScheduler.FromCurrentSynchronizationContext());
+                }
+            }
+        }
+    }
 }
 
