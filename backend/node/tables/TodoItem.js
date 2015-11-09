c2426b83763a4f45963a2d89b1bae28034ba3aca
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2015-11-09 10:55:38 -0800

    Adding turnkey node todo site sample template

---
+++ backend/node/tables/TodoItem.js
@@ -0,0 +1,7 @@
+var table = module.exports = require('azure-mobile-apps').table();
+
+// table.read(function (context) {
+//     return context.execute();
+// });
+
+// table.read.use(customMiddleware, table.operation);
