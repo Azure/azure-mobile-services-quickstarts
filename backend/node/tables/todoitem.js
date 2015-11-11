1825592c933f3229ba187ab9606ef4e009035e11
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2015-11-11 15:19:49 -0800

    Updating todo table script and adding *.js to iisnode watched files list

--- backend/node/tables/todoitem.js
+++ backend/node/tables/todoitem.js
@@ -1,7 +1,42 @@
-var table = module.exports = require('azure-mobile-apps').table();
+// ----------------------------------------------------------------------------
+// Copyright (c) 2015 Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
 
+/*
+** Sample Table Definition - this supports the Azure Mobile Apps
+** TodoItem product with authentication and offline sync
+*/
+var azureMobileApps = require('azure-mobile-apps');
+
+// Create a new table definition
+var table = azureMobileApps.table();
+
+// Configure specific code when the client does a request
+// READ - only return records belonging to the authenticated user
 // table.read(function (context) {
-//     return context.execute();
+//   context.query.where({ userId: context.user.id });
+//   return context.execute();
 // });
 
-// table.read.use(customMiddleware, table.operation);
+// CREATE - add or overwrite the userId based on the authenticated user
+// table.insert(function (context) {
+//   context.item.userId = context.user.id;
+//   return context.execute();
+// });
+
+// UPDATE - for this scenario, we don't need to do anything - this is
+// the default version
+//table.update(function (context) {
+//  return context.execute();
+//});
+
+// DELETE - for this scenario, we don't need to do anything - this is
+// the default version
+//table.delete(function (context) {
+//  return context.execute();
+//});
+
+// Finally, export the table to the Azure Mobile Apps SDK - it can be
+// read using the azureMobileApps.tables.import(path) method
+
+module.exports = table;
