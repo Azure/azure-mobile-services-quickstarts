af956ee319defea28f6df19e6e34a31e88b30d82
Shrirang Shirodkar <shrirs@microsoft.com>
2016-01-07 11:56:46 -0800

    Move required files from phonegap to cordova directory and remove the phonegap directory

--- client/cordova/ZUMOAPPNAME/www/js/index.js
+++ client/cordova/ZUMOAPPNAME/www/js/index.js
@@ -31,21 +31,74 @@ var app = {
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+    // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        
+        var client = new WindowsAzure.MobileServiceClient('ZUMOAPPURL', 'ZUMOAPPKEY');
+        todoItemTable = client.getTable('todoitem');
+
+        // Read current data and rebuild UI.
+        // If you plan to generate complex UIs like this, consider using a JavaScript templating library.
+        function refreshTodoItems() {
+            $('#summary').html("Loading...");
+            var query = todoItemTable.where({ complete: false });
+
+            query.read().then(function(todoItems) {
+                var listItems = $.map(todoItems, function(item) {
+                    return $('<li>')
+                        .attr('data-todoitem-id', item.id)
+                        .append($('<button class="item-delete">Delete</button>'))
+                        .append($('<input type="checkbox" class="item-complete">').prop('checked', item.complete))
+                        .append($('<div>').append($('<input class="item-text">').val(item.text)));
+                });
+
+                $('#todo-items').empty().append(listItems).toggle(listItems.length > 0);
+                $('#summary').html('<strong>' + todoItems.length + '</strong> item(s)');
+            }, handleError);
+        }
+
+        function handleError(error) {
+            var text = error + (error.request ? ' - ' + error.request.status : '');
+            $('#errorlog').append($('<li>').text(text));
+        }
+
+        function getTodoItemId(formElement) {
+            return $(formElement).closest('li').attr('data-todoitem-id');
+        }
+
+        // Handle insert
+        $('#add-item').submit(function(evt) {
+            var textbox = $('#new-item-text'),
+                itemText = textbox.val();
+            if (itemText !== '') {
+                todoItemTable.insert({ text: itemText, complete: false }).then(refreshTodoItems, handleError);
+            }
+            textbox.val('').focus();
+            evt.preventDefault();
+        });
+
+        $('#refresh').click(function(evt) {
+            refreshTodoItems();
+            evt.preventDefault();
+        });
+
+        // Handle update
+        $(document.body).on('change', '.item-text', function() {
+            var newText = $(this).val();
+            todoItemTable.update({ id: getTodoItemId(this), text: newText }).then(null, handleError);
+        });
+
+        $(document.body).on('change', '.item-complete', function() {
+            var isComplete = $(this).prop('checked');
+            todoItemTable.update({ id: getTodoItemId(this), complete: isComplete }).then(refreshTodoItems, handleError);
+        });
+
+        // Handle delete
+        $(document.body).on('click', '.item-delete', function () {
+            todoItemTable.del({ id: getTodoItemId(this) }).then(refreshTodoItems, handleError);
+        });
+
+        // On initial load, start by fetching the current data
+        refreshTodoItems();
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
 };
-
-app.initialize();
